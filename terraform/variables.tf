eaf1b91f9fdf12cd0da2f57df1a6332c34d098db
devops4sure <vl.priyavishnu15@gmail.com>
2026-04-06 00:08:27 +0300

    Fix region to us-east-1

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,11 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-2"
+  default     = "us-east-1
 }
 
 variable "clusterName" {
   description = "Name of the EKS cluster"
   type        = string
-<<<<<<< HEAD
-  default     = "kitops-eks"
-}
-=======
   default     = "vprofile-eks"
 }
-##
->>>>>>> stage
